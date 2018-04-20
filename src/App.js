a8f4c1ebb816799c2bb0c28b26ba43ccfdb6fafb
Jesus Amaya <yokodevmaster2015@gmail.com>
2018-04-20 16:07:51 -0600

    base

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React, { Component } from 'react';
 
-import Base from './Base'
-import TodoList from './TodoList'
-
 class App extends Component {
   render() {
     return (
       <div>
-
-      <Base />
-      <TodoList />
-
+				<h2>Hello there</h2>
       </div>
     );
   }
